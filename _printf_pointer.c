1256ea5dbde4640fe3a0f489678a08352213272c
Otekaye Seun <otekayeseun24@gmail.com>
2023-10-14 14:41:52 -0700

    "printf"

--- _printf_pointer.c
+++ _printf_pointer.c
@@ -8,12 +8,13 @@
 
 int _printf_pointer(va_list args)
 {
-	void *prt = va_arg(args, void*);
-	char *stg = "(null)";
+	void *prt;
+	char *stg = "(nil)";
 	long int fox;
 	int b;
 	int i;
 
+	prt = va_arg(args, void*);
 	if (prt == NULL)
 	{
 		for (i = 0; stg[i] != '\0'; i++)
