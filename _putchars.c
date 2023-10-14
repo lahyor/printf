1bf401e4a3e0ae646beee99ca6d6d5bebef93d00
Otekaye Seun <otekayeseun24@gmail.com>
2023-10-14 09:43:20 -0700

    "printf"

--- _putchars.c
+++ _putchars.c
@@ -8,5 +8,17 @@
 
 int _putchars(char sp)
 {
-	return (write(1, &sp, 1));
+	static char buffer[1024];
+	static int buffer_code;
+
+	if (buffer_code == 1024)
+	{
+		if (write(1, buffer, 1024) != 1024)
+			return (-1);
+		buffer_code = 0;
+	}
+
+	buffer[buffer_code++] = sp;
+
+	return (0);
 }
