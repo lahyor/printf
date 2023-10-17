cf1683959e3b9794ca5733d0a436988651102262
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-10-17 16:05:40 +0100

    num5

---
+++ _printf_spchar5.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_spchar5 - printing hexa as upper case
+ * @quant:this holds the value of the hexas
+ * Return:return len at the end
+ */ 
+
+int print_spchar5(va_list, val)
+{
+	char *e;
+	int k, len = 0;
+	int quant;
+	
+	e = va_arg(val, char *);
+
+	for (k = 0, e[k] != '\0' k++)
+	{
+		if (e[k] < 32 || e[k] >= 127)
+		{
+			_putchars(\\);
+			_putchars('x');
+			len = len + 2;
+			quant = e[i];
+			if (quant < 16)
+			{
+				_putchars('0');
+				len++;
+			}
+			len = len + _printf_HEXA2(quant);
+		}
+		else
+		{
+			_putchars(e[k]);
+			len++;
+		}
+	}
+	return (len);
+}
